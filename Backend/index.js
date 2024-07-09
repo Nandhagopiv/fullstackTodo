7ba2988a16ea52544a6244ca3b83f0d3a3108a23
Nandhagopiv <nandhagopy@gmail.com>
2024-07-09 15:46:58 +0530

    Create(ShoopingList): Created Shopping list todo list using react, express, node, mongodb full stack

---
+++ Backend/index.js
@@ -0,0 +1,76 @@
+import express from 'express';
+import cors from 'cors';
+import { MongoClient } from 'mongodb';
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+const uri = 'mongodb+srv://nandhagopy:12345@clustertodo.fevoqn2.mongodb.net/';
+
+const client = new MongoClient(uri);
+
+server.get('/list', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('todolist');
+    const shoppingList = db.collection('shoppinglist');
+
+    const data = await shoppingList.find().toArray();
+    res.json(data);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ error: 'Failed to fetch shopping list' });
+  }
+});
+
+server.get('/delete', async (req, res) => {
+  try {
+      await client.connect();
+      const db = client.db('todolist');
+      const shoppingList = db.collection('shoppinglist');
+      const itemId = req.query.forDel;
+      
+      const result = await shoppingList.deleteOne({forDel: itemId});
+      console.log(result);
+
+      if (result.deletedCount === 1) {
+        const data = await shoppingList.find().toArray()
+        res.send(data)
+      }
+  } catch (err) {
+      console.error('Error deleting item:', err);
+      res.status(500).json({ error: 'Failed to delete item' });
+  }
+});
+
+server.get('/addlist', async (req, res) => {
+  try {
+    await client.connect();
+    const db = client.db('todolist');
+    const shoppingList = db.collection('shoppinglist');
+
+    const newItem = { item: req.query.item, forDel: req.query.forDel};
+    await shoppingList.insertOne(newItem);
+
+    res.status(201).json({ message: 'Item added to shopping list' });
+  } catch (err) {
+    console.error('Error adding item:', err);
+    res.status(500).json({ error: 'Failed to add item to shopping list' });
+  }
+});
+
+const main = async () => {
+  try {
+    await client.connect();
+    console.log('MongoDB connected successfully');
+
+    server.listen(5000, () => {
+      console.log('Local server is running on port 5000');
+    });
+  } catch (err) {
+    console.error('Failed to connect to MongoDB:', err);
+  }
+};
+
+main();
---
+++ front-end/src/App.js
@@ -0,0 +1,64 @@
+import { Fragment, useEffect, useState } from "react";
+import axios from 'axios'
+
+function App() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  const [product, setProduct] = useState('')
+  const [shoppingList, setShoppingList] = useState([])
+  const [stsDel, setStsDel] = useState(false)
+
+  const handleChange = (e) => {
+    setProduct(e.target.value)
+  }
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/list`).then((data) => {
+      setShoppingList(data.data)
+    })
+  }, [])
+
+  const handleClick = async () => {
+    if (product === '') {
+      alert("Enter something to add to your shopping list")
+    } else {
+      setShoppingList([...shoppingList, { item: product, forDel: shoppingList.length + 1 }])
+      await axios.get(`http://localhost:5000/addlist?item=${product}&forDel=${shoppingList.length + 1}`)
+      setProduct('')
+    }
+  }
+
+  const handleDelete = (id) => {
+    setStsDel(true)
+    axios.get(`http://localhost:5000/delete?forDel=${id}`).then((data) => {
+      setShoppingList(data.data)
+      if (data.statusText === "OK") {
+        setStsDel(false)
+      }
+    })
+  }
+
+  return (
+    <Fragment>
+      <section className="flex flex-col items-center">
+        <form className="w-[80%] sm:w-[50%] flex justify-center gap-2 mt-5" onSubmit={handleSubmit}>
+          <input type="text" className="bg-slate-200 p-2 outline-none rounded-lg" onChange={handleChange} value={product} id="productInput" placeholder="Enter any Product..."></input>
+          <button className="bg-black px-5 py-2 text-white font-bold rounded-lg" onClick={handleClick} type="submit">Add</button>
+        </form>
+        <section className="w-[80%] sm:w-[50%] flex flex-col mt-5 gap-2">
+          {
+            shoppingList.map((data, index) => {
+              return <div className="flex justify-between gap-10 items-center"><h1 className="text-xl font-semibold break-all">{index + 1}. {data.item}</h1><button onClick={() => handleDelete(data.forDel)} className="text-sm font-bold bg-black rounded-lg text-white p-2">Delete</button></div>
+            })
+          }
+        </section>
+        <section style={{ backgroundColor: 'rgba(0, 0, 0, 0.666)', display: stsDel ? 'flex' : 'none' }} className="h-[100%] flex justify-center items-center fixed w-[100%]"><h1 className="text-5xl text-white font-bold">Deleting...</h1></section>
+      </section>
+    </Fragment>
+  );
+}
+
+export default App;
